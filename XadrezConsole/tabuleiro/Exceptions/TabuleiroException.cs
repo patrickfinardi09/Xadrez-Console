a95533e5362e05d5eb7165657065e596d896c323
Patrick Sampaio <patrickfinardi9@gmail.com>
2021-01-14 01:00:23 -0300

    Tratando erros com excecao personalizada

---
+++ XadrezConsole/tabuleiro/Exceptions/TabuleiroException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tabuleiro.Exceptions
+{
+    class TabuleiroException : Exception
+    {
+        public TabuleiroException(string message) : base(message)
+        {
+
+        }
+    }
+}
